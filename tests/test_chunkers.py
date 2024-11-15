72d92aa6e137c0240e062c6ce69c5e57501c77d6
Khaled Sulayman <ksulayma@redhat.com>
2024-11-15 14:30:43 -0500

    Remove tokenizer test files and tests that use them
    Signed-off-by: Khaled Sulayman <ksulayma@redhat.com>

--- tests/test_chunkers.py
+++ tests/test_chunkers.py
@@ -30,56 +30,6 @@ def documents_dir():
     return Path(TEST_DATA_DIR) / "sample_documents"
 
 
-@pytest.fixture
-def tokenizer_model_name():
-    return os.path.join(TEST_DATA_DIR, "models/instructlab/granite-7b-lab")
-
-
-@pytest.mark.parametrize(
-    "filepaths, chunker_type",
-    [
-        ([Path("document.md")], TextSplitChunker),
-        ([Path("document.pdf")], ContextAwareChunker),
-    ],
-)
-def test_chunker_factory(filepaths, chunker_type, documents_dir, tokenizer_model_name):
-    """Test that the DocumentChunker factory class returns the proper Chunker type"""
-    leaf_node = [
-        {
-            "documents": ["Lorem ipsum"],
-            "taxonomy_path": "",
-            "filepaths": filepaths,
-        }
-    ]
-    with tempfile.TemporaryDirectory() as temp_dir:
-        chunker = DocumentChunker(
-            leaf_node=leaf_node,
-            taxonomy_path=documents_dir,
-            output_dir=temp_dir,
-            tokenizer_model_name=tokenizer_model_name,
-        )
-        assert isinstance(chunker, chunker_type)
-
-
-def test_chunker_factory_unsupported_filetype(documents_dir, tokenizer_model_name):
-    """Test that the DocumentChunker factory class fails when provided an unsupported document"""
-    leaf_node = [
-        {
-            "documents": ["Lorem ipsum"],
-            "taxonomy_path": "",
-            "filepaths": [Path("document.jpg")],
-        }
-    ]
-    with pytest.raises(ValueError):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            _ = DocumentChunker(
-                leaf_node=leaf_node,
-                taxonomy_path=documents_dir,
-                output_dir=temp_dir,
-                tokenizer_model_name=tokenizer_model_name,
-            )
-
-
 def test_chunker_factory_empty_filetype(documents_dir):
     """Test that the DocumentChunker factory class fails when provided no document"""
     leaf_node = [
